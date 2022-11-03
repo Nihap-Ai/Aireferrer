6256a478f5da05e14a7e429e51f3210fe0366957
Nihap-Ai <116452375+Nihap-Ai@users.noreply.github.com>
2022-11-03 22:54:14 +0530

    Update views.py

--- apps/processor/views.py
+++ apps/processor/views.py
@@ -6,6 +6,7 @@ from django.conf import settings
 import torch
 from torch import autocast
 from diffusers import StableDiffusionPipeline
+import replicate
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -26,15 +27,26 @@ def form(request):
         
         print(inp)
         
-        access_token = "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"
+       image_urls="nothing"
+def form(request):
+    global image_urls
+    if request.method == 'POST':
+        inp = request.POST.get("inp")
         
-        pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", revision="fp16", torch_dtype=torch.float16, use_auth_token=access_token)
-        pipe = pipe.to("cuda")
-        prompt = inp
-        with autocast("cuda"):
-            image = pipe(prompt)["sample"][0]  
-        image.save("/outpage/astronaut_rides_horse.png") 
-      
+        print(inp)
+       
+       
+        YOUR_API_TOKEN = "9f720a466712b567cd24f7fe177377f2c961018a"
+        PROMPT = inp
+        client = replicate.Client(api_token=YOUR_API_TOKEN)
+        model = client.models.get("stability-ai/stable-diffusion")
+        image_urls = model.predict(prompt=PROMPT)
 
-    return render(request, "home/form.html")
+        
+
+        print(image_urls[0])
+
+    return render(request, "home/form.html",{"image_urls": image_urls[0]})
+
+ 
 
