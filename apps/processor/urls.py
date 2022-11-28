fe6796dfb79a9aaa1cdb0c086250bb5af82c946b
Nihap-Ai <116452375+Nihap-Ai@users.noreply.github.com>
2022-11-28 15:54:55 +0530

    Update urls.py

--- apps/processor/urls.py
+++ apps/processor/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from . import views
 from .views import *
 
@@ -8,5 +8,9 @@ urlpatterns = [
     path("Art-Generator/", form, name="Art-Generator"),
     path('contact/', views.contact, name='contact'),
     path('about-us/', views.about, name='about'),
+    path('Art-output/', views.output, name='art_output'),
+    path('Donate/', views.donate, name='donate'),
+
+
 
 ]
