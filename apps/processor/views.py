a564ab7b880e135320a8cc48998a9f5b1940c2e0
Nihap-Ai <116452375+Nihap-Ai@users.noreply.github.com>
2022-12-01 20:53:53 +0530

    Update views.py

--- apps/processor/views.py
+++ apps/processor/views.py
@@ -7,6 +7,9 @@ import torch
 from torch import autocast
 from diffusers import StableDiffusionPipeline
 import replicate
+import time
+import mysql.connector
+
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -14,6 +17,31 @@ class HomeView(TemplateView):
 
     
 def contact(request):
+    if request.method == 'POST':
+        fname = request.POST.get("fname")
+        mail = request.POST.get("mail")
+        msg = request.POST.get("msg")
+        dataBase = mysql.connector.connect(
+            host ="containers-us-west-136.railway.app",
+            user ="root",
+            passwd ="qlK7pZsyw72HKjrvxGU7",
+            database ="railway",
+            port = "6330"
+            )
+        cursorObject = dataBase.cursor()
+   
+        query = "INSERT INTO Contacts (FullName,Mail,Message)\
+        VALUES (%s, %s, %s)"
+        val = (fname,mail,msg)
+             
+        cursorObject.execute(query,val)
+        dataBase.commit()
+
+
+        
+
+
+
     return render(request, 'home/page-contact-us.html')
 
 def about(request):
@@ -24,12 +52,16 @@ def donate(request):
 
 
 
+ 
 
-
-
+def form(request):
+    if request.method == 'POST':
+        inp = request.POST.get("inp")
+        
+        print(inp)
         
 image_urls=['../../static/assets/img/temple.jpg','nothing']
-RPROMPT = 'Nothing'
+RPROMPT = "nothing"
 PROMPT="temple in ruines, forest, stairs, columns, cinematic, detailed, atmospheric, epic, concept art, Matte painting, background, mist, photo-realistic, concept art, volumetric light, cinematic epic + rule of thirds octane render, 8k, corona render, movie concept art, octane render, cinematic, trending on artstation, movie concept art, cinematic composition , ultra-detailed, realistic , hyper-realistic , volumetric lighting, 8k –ar 2:3 –test –uplight"
 img_name = PROMPT[:12]
 def form(request):
@@ -39,24 +71,47 @@ def form(request):
         inp = request.POST.get("inp")
         
         print(inp)
-       
+      # DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'railway',
+       # 'USER': 'root',
+       # 'PASSWORD': 'qlK7pZsyw72HKjrvxGU7',
+        #'HOST':'containers-us-west-136.railway.app',
+        #'PORT':'6330',
+    #}
+#}
        
         YOUR_API_TOKEN = "9f720a466712b567cd24f7fe177377f2c961018a"
         PROMPT = inp
         RPROMPT = "mdjrny-v4 style a highly detailed art of "+PROMPT+", 4 k resolution, trending on artstation, masterpiece, smooth, sharp focus,illustration, 8k,Super-Resolution, hyper realistic, super detailed,concept art"
-        client = replicate.Client(api_token=YOUR_API_TOKEN)
-        model = client.models.get("stability-ai/stable-diffusion")
-        image_urls = model.predict(prompt=RPROMPT)
+         
+        image_urls = ['https://replicate.delivery/pbxt/dcekfwYQnGtxFUUbXfLzn0ifRlesxe6QmOEB0FDpoOA6fDgCIA/out-0.png','noi']
         img_name = PROMPT[:12]
- 
         
-
         print(image_urls[0])
 
+        dataBase = mysql.connector.connect(
+            host ="containers-us-west-136.railway.app",
+            user ="root",
+            passwd ="qlK7pZsyw72HKjrvxGU7",
+            database ="railway",
+            port = "6330"
+            )
+        cursorObject = dataBase.cursor()
+   
+        query = "INSERT INTO datas (url,prompt)\
+        VALUES (%s, %s)"
+        val = (image_urls[0],inp)
+             
+        cursorObject.execute(query,val)
+        dataBase.commit()
+
     return render(request, "home/Art-Generator.html",{"image_urls": image_urls[0], "PROMPT": PROMPT,"img_name": img_name, "RPROMPT": RPROMPT})
+
  
 
 def output(request):
     
 
-    return render(request, 'home/Art-generator-output.html',{"image_urls": image_urls[0], "PROMPT": PROMPT,"img_name": img_name, "RPROMPT": RPROMPT})
+    return render(request, 'home/Art-generator-output.html',{"image_urls": image_urls[0], "PROMPT": PROMPT,"img_name": img_name, "RPROMPT": RPROMPT}) 
