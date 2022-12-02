58a6acd176aa65bd0b1df15027d9c3188e2def1b
Nihap-Ai <116452375+Nihap-Ai@users.noreply.github.com>
2022-12-02 20:54:35 +0530

    Update views.py

--- apps/processor/views.py
+++ apps/processor/views.py
@@ -84,10 +84,11 @@ def form(request):
        
         YOUR_API_TOKEN = "9f720a466712b567cd24f7fe177377f2c961018a"
         PROMPT = inp
-        RPROMPT = "mdjrny-v4 style a highly detailed art of "+PROMPT+", 4 k resolution, trending on artstation, masterpiece, smooth, sharp focus,illustration, 8k,Super-Resolution, hyper realistic, super detailed,concept art"
-         
-        image_urls = ['https://replicate.delivery/pbxt/dcekfwYQnGtxFUUbXfLzn0ifRlesxe6QmOEB0FDpoOA6fDgCIA/out-0.png','noi']
+        client = replicate.Client(api_token=YOUR_API_TOKEN)
+        model = client.models.get("stability-ai/stable-diffusion")
+        image_urls = model.predict(prompt=PROMPT)
         img_name = PROMPT[:12]
+
         
         print(image_urls[0])
 
