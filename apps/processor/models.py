c28863c5ed53d25f0fc466359ffb20f0905f0111
Nihap-Ai <116452375+Nihap-Ai@users.noreply.github.com>
2022-12-03 20:27:56 +0530

    Update models.py

--- apps/processor/models.py
+++ apps/processor/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Article(models.Model):
+	title = models.CharField(max_length=200)
+	lastedit_date = models.DateTimeField()
+
+	def get_absolute_url(self):
+		return "/p/%i/" % self.id
+
 # Create your models here.
