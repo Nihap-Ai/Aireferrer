9881f0599ec574dbf622ec0d269e50403af69a95
Nihap-Ai <116452375+Nihap-Ai@users.noreply.github.com>
2022-12-03 23:23:56 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+etnck%&$k55epg!f$!g+j(wiu*q1ia=)v#ex$$h^u1!jdma)2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
