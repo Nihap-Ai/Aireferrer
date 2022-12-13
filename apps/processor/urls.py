6a7d65e0bf1273a6411fc60e9b6211102652f154
Nihap-Ai <116452375+Nihap-Ai@users.noreply.github.com>
2022-12-13 17:58:41 +0530

    Update urls.py

--- apps/processor/urls.py
+++ apps/processor/urls.py
@@ -1,24 +1,24 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path,re_path
 from . import views
 from .views import *
 
 
 
 
-
 urlpatterns = [
     path("", views.HomeView.as_view(), name="HomeView"),
-    path("Art-Generator/", form, name="Art-Generator"),
-    path('contact/', views.contact, name='contact'),
-    path('about-us/', views.about, name='about'),
+    path("Art-Generator", form, name="Art-Generator"),
+    path('contact', views.contact, name='contact'),
+   # path('about-us/', views.about, name='about'),
+    re_path(r"^about-us$", views.about, name="about"),
     path('Art-output/', views.output, name='art_output'),
-    path('Donate/', views.donate, name='donate'),
-   
+    path('Donate', views.donate, name='donate'),
+    path('sitemap.html', views.sitemap, name = 'sitemap'),
+
 
     
     
 
 
-] 
- 
+]
