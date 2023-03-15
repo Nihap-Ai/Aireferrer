a75b3ac686708a7c39c7bc97a64ddbdce96add97
Nihap-Ai <116452375+Nihap-Ai@users.noreply.github.com>
2023-03-15 21:39:09 +0530

    Update views.py

--- apps/processor/views.py
+++ apps/processor/views.py
@@ -97,42 +97,14 @@ def form(request):
     #}
 #}
        
-        YOUR_API_TOKEN = "9f720a466712b567cd24f7fe177377f2c961018a"
+         YOUR_API_TOKEN = "9f720a466712b567cd24f7fe177377f2c961018a"
         PROMPT = inp
         RPROMPT = "mdjrny-v4 style a highly detailed art of "+PROMPT+", 4 k resolution, trending on artstation, masterpiece, smooth, sharp focus,illustration, 8k,Super-Resolution, hyper realistic, super detailed,concept art"
         
-        model = replicate.models.get("prompthero/openjourney")
+        client = replicate.Client(api_token=YOUR_API_TOKEN)
+        model = client.models.get("prompthero/openjourney")
         version = model.versions.get("9936c2001faa2194a261c01381f90e65261879985476014a0a37a334593a05eb")
-
-# https://replicate.com/prompthero/openjourney/versions/9936c2001faa2194a261c01381f90e65261879985476014a0a37a334593a05eb#input
-        inputs = {
-         'prompt': "mdjrny-v4 style a highly detailed matte painting of a man on a hill watching a rocket launch in the distance by studio ghibli, makoto shinkai, by artgerm, by wlop, by greg rutkowski, volumetric lighting, octane render, 4 k resolution, trending on artstation, masterpiece",
-
-       # Width of output image. Maximum size is 1024x768 or 768x1024 because
-       # of memory limits
-         'width': 512,
-
-       # Height of output image. Maximum size is 1024x768 or 768x1024 because
-       # of memory limits
-         'height': 512,
-
-       # Number of images to output
-         'num_outputs': 1,
-   
-       # Number of denoising steps
-       # Range: 1 to 500
-         'num_inference_steps': 50,
-
-       # Scale for classifier-free guidance
-       # Range: 1 to 20
-         'guidance_scale': 6,
-
-       # Random seed. Leave blank to randomize the seed
-       # 'seed': ...,
-       }
-
-     # https://replicate.com/prompthero/openjourney/versions/9936c2001faa2194a261c01381f90e65261879985476014a0a37a334593a05eb#output-schema
-       output = version.predict(**inputs)
+        image_urls = model.predict(prompt=RPROMPT,num_outputs=4)
         
 
         img_name = PROMPT[:12]
@@ -144,8 +116,7 @@ def form(request):
         img3 = image_urls[2]
         img4 = image_urls[3]
         
-        print(output)
-
+        print(img)
 
   
 
